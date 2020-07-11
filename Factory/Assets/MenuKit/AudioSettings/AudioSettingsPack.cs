088df6924e39b29a26f63bd2a585c414e5fa9a04
Igo0Go <chudakin98@gmail.com>
2020-07-11 13:19:21 +0700

    [Проект временно не работает] Реструктуризация проекта + изменение принипа работы скрипта AudioVolumeChanger

--- Factory/Assets/MenuKit/AudioSettings/AudioSettingsPack.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class AudioSettingsPack 
-{
-    public static float musicMultiplicator;
-    public static float otherAudioMultiplicator;
-}
